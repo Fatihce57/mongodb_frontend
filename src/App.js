1c50e042407690845a28e44efaaf922681d71aa3
Fatih <fatihce57@gmail.com>
2021-11-22 15:33:52 +0100

    add user created

--- src/App.js
+++ src/App.js
@@ -7,14 +7,18 @@ const backendUrl = process.env.REACT_APP_BACKEND_URL;
 
 function App() {
 	const [users, setUsers] = useState([]);
+const [isAddingUser, setIsAddingUser] = useState(false);
+	const [formName, setFormName] = useState('');
+	const [formUsername, setFormUsername] = useState('');
+	const [formEmail, setFormEmail] = useState('');
 
-	// const backendUrl = 'http://localhost:3016';
-
-	const loadUsers = async () => {
-		const response = await fetch(backendUrl);
-		const users = await response.json();
-		users.forEach(user => user.isEditingEmail = false);
-		setUsers(users);
+	const loadUsers = () => {
+		(async () => {
+			const response = await fetch(backendUrl);
+			const users = await response.json();
+			users.forEach(user => user.isEditingEmail = false);
+			setUsers(users);
+		})();
 	}
 
 	useEffect(() => {
@@ -51,32 +55,115 @@ function App() {
 			});
 			loadUsers();
 	}
+	const clearForm = () => {
+		setFormName('');
+		setFormUsername('');
+		setFormEmail('');
+	}
+
+	const handleToggleAddUserArea = () => {
+		setIsAddingUser(!isAddingUser);
+	}
+
+	const handleCancelAddForm = (e) => {
+		e.preventDefault();
+		clearForm();
+		setIsAddingUser(!isAddingUser);
+	}
+
+	const handleFormName = (e) => {
+		setFormName(e.target.value);
+	}
+
+	const handleFormUsername = (e) => {
+		setFormUsername(e.target.value);
+	}
+
+	const handleFormEmail = (e) => {
+		setFormEmail(e.target.value);
+	}
+
+	const handleFormSaveButton = (e) => {
+		e.preventDefault();
+		(async () => {
+			await fetch(`${backendUrl}/insertuser`, {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify({
+					user: {
+						name: formName,
+						username: formUsername,
+						email: formEmail
+					}
+				})
+			});
+			clearForm();
+			setIsAddingUser(false);
+			loadUsers();
+		})();
+	}
 
 	return (
 		<div className="App">
 			<h1>User Management App</h1>
-			<div className="topRow">
-				<button>Add User</button>
+			<div className="addUserArea">
+				<div className="topInfoRow">
+					<button onClick={handleToggleAddUserArea}>Add User</button>
+					<div className="totalInfo">Total: {users.length} users</div>
+				</div>
+				{isAddingUser && (
+					<div className="addUserFormArea">
+						<form>
+							<div className="row">
+								<label htmlFor="name">Full Name: </label>
+								<input type="text"
+									value={formName}
+									onChange={handleFormName}
+									id="name" />
+							</div>
+							<div className="row">
+								<label htmlFor="username">User Name: </label>
+								<input type="text"
+									value={formUsername}
+									onChange={handleFormUsername}
+									id="username" />
+							</div>
+
+							<div className="row">
+								<label htmlFor="email">Email: </label>
+								<input type="text"
+									value={formEmail}
+									onChange={handleFormEmail}
+									id="email" />
+							</div>
+
+							<div className="formButtonArea">
+								<button onClick={(e) => handleFormSaveButton(e)}>Save New User</button>
+								<button onClick={handleCancelAddForm}>Cancel</button>
+							</div>
+						</form>
+					</div>
+				)}
 			</div>
 			<section className="users">
 				{users.map((user, index) => {
 					return (
 						<div key={index} className="userCard">
 							<div className="row">
-								<div className="label">Full Name:</div>
+								<div className="label">Full Name: </div>
 								<div className="data">{user.name}</div>
 							</div>
 							<div className="row">
-								<div className="label">User Name:</div>
+								<div className="label">User Name: </div>
 								<div className="data">{user.username}</div>
 							</div>
 							<div className="row">
-								<div className="label">E-Mail:</div>
+								<div className="label">E-Mail: </div>
 								{!user.isEditingEmail && (
 									<div className="data">{user.email}</div>
 								)}
 								{user.isEditingEmail && (
-									<div className="data editing"><input type="text" onChange={(e) => handleEmailChange(user, e)} value={user.email} /><button onClick={() => handleEmailSave(user)}>Save</button><button onClick={() => handleEditCancelButton(user)}>Cancel</button></div>
+									<div className="data editing"><input type="text" onChange={(e) => handleEmailChange(user, e)} value={user.email} /><button onClick={() => handleEmailSave(user)}>Save</button><button onClick={() => handleEditButton(user)}>Cancel</button></div>
 								)}
 							</div>
 							<div className="iconRow">
